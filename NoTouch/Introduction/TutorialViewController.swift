c7cea3fecdcfff99aa4692c6e4f2ab79bd3b4905
apmason <apmason119@gmail.com>
2020-03-07 18:43:27 -0500

    Basis of tutorial scroll view

--- NoTouch/Introduction/TutorialViewController.swift
+++ NoTouch/Introduction/TutorialViewController.swift
@@ -11,22 +11,50 @@ import UIKit
 class TutorialViewController: UIViewController {
 
     @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var pageControl: UIPageControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let slides = createSlides()
+        setupSlideScrollView(slides: slides)
+        
+        pageControl.numberOfPages = slides.count
+        pageControl.currentPage = 0
+        view.bringSubviewToFront(pageControl)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func createSlides() -> [SlideView] {
+        guard let slideOne: SlideView = Bundle.main.loadNibNamed("SlideView", owner: self, options: nil)?.first as? SlideView else {
+            assertionFailure("You fucked up")
+            return []
+        }
+        
+        //slideOne.textLabel.text = //Localized text
+        
+        guard let slideTwo: SlideView = Bundle.main.loadNibNamed("SlideView", owner: self, options: nil)?.first as? SlideView else {
+            assertionFailure("You fucked up")
+            return []
+        }
+        slideTwo.textLabel.text = "Move the device closer to your face for best results."
+        
+        return [slideOne, slideTwo]
     }
-    */
+    
+    func setupSlideScrollView(slides : [SlideView]) {
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+        scrollView.isPagingEnabled = true
+        
+        for i in 0 ..< slides.count {
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+            scrollView.addSubview(slides[i])
+        }
+    }
+}
 
+extension TutorialViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        pageControl.currentPage = Int(scrollView.contentOffset.x / view.bounds.width)
+    }
 }
