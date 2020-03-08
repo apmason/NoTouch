18f84657595eb6196cf2a144c8270c878d80aa3a
apmason <apmason119@gmail.com>
2020-03-08 13:06:23 -0400

    Don't let app go to sleep

--- NoTouch/AppDelegate.swift
+++ NoTouch/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        UIApplication.shared.isIdleTimerDisabled = true
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let initialViewController: UIViewController
