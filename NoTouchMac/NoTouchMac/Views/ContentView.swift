2099647743c5b087e185732fb1972d6612d6a6ff
apmason <apmason119@gmail.com>
2020-04-25 19:45:36 -0400

    Add new logo and button for hiding camera

--- NoTouchMac/NoTouchMac/Views/ContentView.swift
+++ NoTouchMac/NoTouchMac/Views/ContentView.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Canopy Interactive. All rights reserved.
 //
 
+import AppKit
 import SwiftUI
 import NoTouchCommon
 
@@ -14,23 +15,44 @@ struct ContentView: View {
     @EnvironmentObject var userSettings: UserSettings
         
     //let contentViewModel = ContentViewModel()
+    private var cameraImageAspectRatio: CGFloat {
+        let image = NSImage(named: "camera")!
+        let aspect = image.size.width / image.size.height
+        return aspect
+    }
     
     var body: some View {
         ZStack(alignment: .topLeading) {
             VideoLayerView()
-            Button(action: {
-                self.userSettings.muteSound.toggle()
-            }) {
-                Image(userSettings.muteSound ? "speaker.slash" : "speaker")
-                    .resizable()
-                    .padding(8)
-                    .foregroundColor(Color.white)
-                    .background(Color.black.opacity(0.75))
-                    .cornerRadius(10)
+            VStack {
+                Button(action: {
+                    self.userSettings.muteSound.toggle()
+                }) {
+                    Image(userSettings.muteSound ? "speaker.slash" : "speaker")
+                        .resizable()
+                        .padding(8)
+                        .foregroundColor(Color.white)
+                        .background(Color.black.opacity(0.75))
+                        .cornerRadius(10)
+                }
+                .buttonStyle(PlainButtonStyle())
+                .frame(width: 50, height: 50)
+                .padding(10)
+                
+                Button(action: {
+                    self.userSettings.hideCameraFeed.toggle()
+                }) {
+                    Image(userSettings.hideCameraFeed ? "eye.slash" : "eye")
+                        .resizable()
+                        .padding(4)
+                        .foregroundColor(Color.white)
+                        .background(Color.black.opacity(0.75))
+                        .cornerRadius(10)
+                }
+                .buttonStyle(PlainButtonStyle())
+                .frame(width: 50, height: 50, alignment: .center)
+                .padding(10)
             }
-            .buttonStyle(PlainButtonStyle())
-            .frame(width: 40, height: 40, alignment: .topLeading)
-            .padding(10)
         }
     }
 }
