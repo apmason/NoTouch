c7f11983e0857e9393ccb220d96c1617a890da67
apmason <apmason119@gmail.com>
2020-05-12 08:12:12 -0400

    Make x axis zero-indexed

--- NoTouchCommon/NoTouchCommon/Views/Graph/GraphXLabels.swift
+++ NoTouchCommon/NoTouchCommon/Views/Graph/GraphXLabels.swift
@@ -14,23 +14,23 @@ struct GraphXLabels: View {
     var body: some View {
         GeometryReader { geometry in
             Group {
-                Text("6am")
+                Text("12am")
                     .frame(width: 40, height: 40, alignment: .leading)
                     .position(x: self.positioner.leadingXOffset + 20,
                               y: geometry.size.height - (self.positioner.bottomYOffset / 2))
                 
-                Text("12pm")
+                Text("6am")
                     .frame(width: 40, height: 40, alignment: .center)
                     .position(x: self.positioner.xAxisLabelOffsetFor(line: 1, contentWidth: geometry.size.width),
                               y: geometry.size.height - (self.positioner.bottomYOffset / 2))
                 
-                Text("6pm")
+                Text("12pm")
                     .frame(width: 40, height: 40, alignment: .center)
                     .position(x: self.positioner.xAxisLabelOffsetFor(line: 2,
                                                                      contentWidth: geometry.size.width),
                               y: geometry.size.height - (self.positioner.bottomYOffset / 2))
                 
-                Text("12am")
+                Text("6pm")
                     .frame(width: 40, height: 40, alignment: .center)
                     .position(x: self.positioner.xAxisLabelOffsetFor(line: 3,
                                                                      contentWidth: geometry.size.width),
