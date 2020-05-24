0e00cbab959415d5e975e78cf047b26146475488
Alex Mason <apmason119@gmail.com>
2020-05-24 06:10:16 -0400

    Add iOS project directly to project

---
+++ HandsOff-iOS/HandsOff-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HandsOff-iOS
+//
+//  Created by Alexander Mason on 5/24/20.
+//  Copyright © 2020 Canopy Interactive. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
